c3d5e525ab7d6caa2c2a06471d7152fb05b14e04
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2023-01-13 21:03:21 +0100

    Intermediate Commit

--- frontend-image-retrival/src/App.js
+++ frontend-image-retrival/src/App.js
@@ -5,6 +5,9 @@ import Filters from './components/Filters.jsx';
 function App() {
   return (
     <>
+        <div>
+          <h2 className="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600 text-center">Image Aesthetics Retrival</h2>
+          </div>
         <div class="flex flex-row bg-gray-50">
           <div class="basis-1/2">
             <ImageUploader/>
