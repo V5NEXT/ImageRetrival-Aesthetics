c93253eef7a5717314cb92e8c9cf251291c0fa12
Ekaterina Baikova <katebay3@gmail.com>
2023-01-20 16:22:58 +0100

    extract_features_image and features_to_kmeans_pca

--- backend/features_to_kmeans_pca.py
+++ backend/features_to_kmeans_pca.py
@@ -1,26 +1,64 @@
 from sklearn.cluster import KMeans
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+import json
+import random 
 
-X = np.array(["upload features"])
-# Define the number of clusters
-n_clusters = 4
-# Create a KMeans object and fit the data
-kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(X)
-# Get the cluster assignments for each data point
-clusters = kmeans.predict(X)
+def featuresToKmeansPCA(json_path, n_images, n_clusters):
+    # Json data path (json: {"filenames" : [] , "features" : []} )
+    # json_path = "features/features_color.json"
+    # json_path = "features/features_InceptionV3.json"
+    # json_path = "features/features_ResNet50.json"
 
+    indexes = random.sample(range(0, 2856), n_images)
+    indexes.sort()
 
-# Plot the data points colored by cluster assignment
-plt.scatter(X[:, 0], X[:, 1], c=clusters, cmap='rainbow')
-# Plot the cluster centroids
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker='x', c='black')
-plt.show()
+    # Upload file
+    data = json.load(open(json_path))
 
-# Perform PCA on the features
-pca = PCA(n_components=5)
-principal_components = pca.fit_transform(X)
-# Plot the clusters using PCA
-plt.scatter(principal_components[:, 0], principal_components[:, 1], c=clusters)
-plt.show()
+    filenames = data["filenames"]
+    X = data["features"]
+
+    filenames = np.array(filenames)[np.array(indexes)]
+    filenames = filenames.tolist() ##########################
+
+    X =  np.array(X)[np.array(indexes)]
+    X = X.tolist()
+
+    # Create a KMeans object and fit the data
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(X)
+
+    # Get the cluster assignments for each data point
+    clusters = kmeans.predict(X)
+
+    cluster_centers = list(zip(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1])) 
+
+    # Perform PCA on the features
+    pca = PCA(n_components=5)
+    principal_components = pca.fit_transform(X)
+
+    # x and y-axes correspond to the first two principal components.
+    # x_coord = principal_components[:, 0]
+    # y_coord = principal_components[:, 1]
+
+    coordinates = list(zip(principal_components[:, 0], principal_components[:, 1])) ##########################
+
+    cluster_centers = pca.transform(kmeans.cluster_centers_) 
+    cluster_centers_list = [np_array.tolist() for np_array in cluster_centers] ##########################
+
+    # # Plot the clusters using PCA
+    # plt.scatter(principal_components[:, 0], principal_components[:, 1], c=clusters)
+    # plt.scatter(cluster_centers[:, 0], cluster_centers[:, 1], color="red")
+    # plt.show()
+
+    clusters_list = clusters.tolist() ##########################
+
+    output = {"filenames" : filenames,
+            "coordinates" : coordinates,
+            "cluster_assignments" : clusters_list,
+            "cluster_centers" : cluster_centers_list
+            }
+    json_string = json.dumps(output)
+    
+    return json_string
